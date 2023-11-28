5fbdf9e27aba6069037bdd7800fc5fd2b07ce6a6
Albert Karagulov <albert.karagulov@mail.ru>
2023-11-28 11:25:14 +0300

    created tourist account page

--- src/pages/TouristPage/index.jsx
+++ src/pages/TouristPage/index.jsx
@@ -6,6 +6,39 @@ export const TouristPage = () => {
   return (
     <div className={styles.tourist_page}>
       <TouristNav />
+      <div className={styles.tourist_pageBlock}>
+        <h3 className={styles.tourist_pageBlock__title}>Аккаунт</h3>
+        <p className={styles.tourist_pageBlock__text}>Фио, почта</p>
+        <div className={styles.tourist_pageBlock__stats}>
+          <div className={styles.tourist_pageBlock__statsItem}>
+            <p className={styles.tourist_pageBlock__statsItem__number}>0</p>
+            <p className={styles.tourist_pageBlock__statsItem__text}>
+              Количество туров
+            </p>
+          </div>
+          <div className={styles.tourist_pageBlock__statsItem}>
+            <p className={styles.tourist_pageBlock__statsItem__number}>0</p>
+            <p className={styles.tourist_pageBlock__statsItem__text}>
+              Количество регионов
+            </p>
+          </div>
+        </div>
+        <hr className={styles.tourist_pageBlock__line} />
+        <div className={styles.tourist_pageBlock__request}>
+          <h3 className={styles.tourist_pageBlock__requestTitle}>
+            Индивидуальный формат путешествий своей компанией!
+          </h3>
+          <p className={styles.tourist_pageBlock__requestText}>
+            Если вы хотите отправиться в авторский тур только своей семьей,
+            компанией друзей или единомышленников по уникальной программе,
+            заполните заявку, и наши проверенные тревел-эксперты подготовят
+            предложение специально для вас.
+          </p>
+          <button className={styles.tourist_pageBlock__requestBtn}>
+            Заполнить заявку
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
